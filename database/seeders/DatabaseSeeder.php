3663cc791eae6394bc783c19311fbccac552c926
AlesciDavide <davi.alesci@Gmail.com>
2024-07-31 13:05:49 +0200

    creato modello il migrate  e il seeder per le tecnologie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypesSeeder::class,
+            TechnologySeeder::class,
             ProjectSeeder::class,
         ]);
     }
